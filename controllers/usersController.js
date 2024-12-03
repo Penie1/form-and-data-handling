746f76f2757693d57a9eb1603a0ca5c5f7f87f12
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-12-03 21:26:29 +0300

    Add router to add user

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,3 +3,11 @@ const usersStorage = require("../storages/usersStorage");
 exports.getAllUsers = (req, res) => {
   res.render("index", { users: usersStorage.getUsers() });
 };
+exports.getCreateUserForm = (req, res) => {
+  res.render("createUser");
+};
+exports.createUsers = (req, res) => {
+  const { firstName, lastName } = req.body;
+  usersStorage.addUsers({ firstName, lastName });
+  res.redirect("/");
+};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -3,5 +3,6 @@ const usersController = require("../controllers/usersController");
 const userRouter = Router();
 
 userRouter.get("/", usersController.getAllUsers);
-
+userRouter.get("/create", usersController.getCreateUserForm);
+userRouter.post("/create", usersController.createUsers);
 module.exports = userRouter;
