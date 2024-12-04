56c595d4226fe28fe1e7b02ed93b3243bcbda91d
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-12-04 10:30:22 +0300

    Validate user input

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,16 +1,44 @@
 const usersStorage = require("../storages/usersStorage");
-
+const { body, validationResult } = require("express-validator");
+const alphError = "must only contains letters.";
+const lengthError = "must be between 1 to 10 characters.";
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphError}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthError}`),
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphError}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthError}`),
+];
 exports.getAllUsers = (req, res) => {
   res.render("index", { users: usersStorage.getUsers() });
 };
 exports.getCreateUserForm = (req, res) => {
   res.render("createUser");
 };
-exports.createUsers = (req, res) => {
-  const { firstName, lastName } = req.body;
-  usersStorage.addUsers({ firstName, lastName });
-  res.redirect("/");
-};
+exports.createUsers = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+    const { firstName, lastName } = req.body;
+    if (!errors.isEmpty()) {
+      return res.status(400).render("createUser", {
+        firstName: firstName,
+        lastName: lastName,
+        errors: errors.array(),
+      });
+    }
+
+    usersStorage.addUsers({ firstName, lastName });
+    res.redirect("/");
+  },
+];
 exports.getUpdateUserForm = (req, res) => {
   const { id } = req.params;
   const user = usersStorage.storage[id];
